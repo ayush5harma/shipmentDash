4aa683c496584698315640824b341adc42f92126
ayush5harma <47772616+ayush5harma@users.noreply.github.com>
2020-12-06 12:30:49 +0530

    TableContainer an Counters added and styled

--- src/components/timeline.jsx
+++ src/components/timeline.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import scan from './scan.jsx';
 import TimelineItem from "./Timelineitem";
 const src = "./FrontendAssets/warehouse.svg"
-const dest = "./FrontAssets/destination.svg"
+const dest = "./FrontendAssets/destination.svg"
 const Timeline = () =>
     scan.length > 0 && (
         <div>
-            <img src={dest} className='Image destinationImage' />
+            <div className="CircleBackground"><img src={dest} className='Image destinationImage' /></div>
         <div className="timeline-container">
             {scan.map((scan, idx) => (
                 <TimelineItem scan={scan} key={idx} />
             ))}
         </div>
-            <img src={src} className='Image sourceImage'/>
+            <div className="CircleBackground"><img src={src} className='Image sourceImage'/></div>
         </div>
     );
 export default Timeline;
